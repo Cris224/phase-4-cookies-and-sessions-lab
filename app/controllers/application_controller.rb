b8b1c2a49f51bc02e9f89852468685cbe45f8271
Cristofer <cristoferlorenzana@gmail.com>
2022-01-05 11:45:28 -0600

    commit tree 5fe4ba561327e224922044a19ada88eddd14f529

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def show
+    session[:page_views] ||= 0
+    session[:page_views] + 1
+    if session[:page_views] < 4
+    render json: { session: session, cookies: cookies.to_hash }
+    else
+      render json: error, status: :unautorized
+    end
+  end
+
 end
